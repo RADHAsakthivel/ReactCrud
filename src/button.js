8d61ecbbffcdd7c2e778a39fdc1222d43b9bad43
RADHAsakthivel <sakthi9819@gmail.com>
2022-10-05 18:33:05 +0530

    created the backend json

--- src/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const  Button = ({count,clickHandler}) => {
-  return (
-    <button onClick={clickHandler}>{count} time clicked </button>
-  )
-}
-
-export default Button
